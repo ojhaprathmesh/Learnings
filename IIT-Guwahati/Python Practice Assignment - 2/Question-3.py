8b0f229e9455bd8b246e8e842c3a8325c4b78d94
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2025-02-23 18:52:23 +0530

    Added comment for clarity

--- IIT-Guwahati/Python Practice Assignment - 2/Question-3.py
+++ IIT-Guwahati/Python Practice Assignment - 2/Question-3.py
@@ -1,6 +1,6 @@
 from random import choice
 
-result = [0 if choice(["heads", "tails"]) == "heads" else 1 for _ in range(10)]
+result = [0 if choice(["heads", "tails"]) == "heads" else 1 for _ in range(10)]  # Creates a list of 0s and 1s
 
 heads = result.count(0)
 tails = 10 - heads
