53fa2d7d1aec0f272208293738bd6f70670ebe11
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2025-03-08 19:44:19 +0530

    Question-1

---
+++ _University/Sem IV/Seminar 2025/Assignment-3/Arrays-Wave Print Row Wise.py
@@ -0,0 +1,17 @@
+def toArray():
+	return list(map(int, input().split()))
+
+m, n = toArray()
+array = [[] for _ in range(m)]
+
+for i in range(m):
+	array[i] = toArray()
+
+for row in range(m):
+	for col in range(n):
+		if row % 2 == 0:
+			print(array[row][col], end=", ")
+		else:
+			print(array[row][n - col - 1], end=", ")
+
+print("END")
