7f9de87eab5d962dd3d7ecdd3c8d3f4380f11dfe
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2025-06-02 18:54:15 +0530

    Flutter: Learning flutter!

---
+++ Flutter/FirstProject/android/app/src/main/java/learning/flutter/first_project/MainActivity.java
@@ -0,0 +1,6 @@
+package learning.flutter.first_project;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ Flutter/FirstProject/lib/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const green = Color(0xFF07B2A8);
+}
