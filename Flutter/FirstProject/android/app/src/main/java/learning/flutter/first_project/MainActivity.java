6eeca225cadbbe6cc18645985448f6c5e3179759
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2025-07-01 21:06:37 +0530

    Dart: Basics

--- Flutter/FirstProject/android/app/src/main/java/learning/flutter/first_project/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package learning.flutter.first_project;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- Flutter/FirstProject/lib/constants/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const green = Color(0xFF07B2A8);
-}
